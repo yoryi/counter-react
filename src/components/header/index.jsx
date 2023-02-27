290fac3faa6ed0b768b8003178039d3e767c5709
yoryi <yoryi.ojeda@aule.co>
2023-02-27 16:09:43 -0500

    Add: project configuration

---
+++ src/components/header/index.jsx
@@ -0,0 +1,9 @@
+import './styles.css'
+function Header() {
+  return (
+    <div className='😀'>
+      <p>Contador - React</p>
+    </div>
+  );
+}
+export default Header;
---
+++ src/module/counter/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+//creates an initialized state = 0 (hooks = useState)
+
+//create two buttons to increase and decrease the state
+
+function Counter() {
+  return (
+    <div>
+    </div>
+  );
+}
+export default Counter;
